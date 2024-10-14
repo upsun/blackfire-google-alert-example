07e66aad017e70049394f22e5c402c8832eaaca8
Florent Huck <flovntp@gmail.com>
2024-10-14 16:09:07 +0200

    new try

--- migrations/Version20241014135933.php
+++ migrations/Version20241014135933.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241014135932 extends AbstractMigration
+final class Version20241014135933 extends AbstractMigration
 {
     public function getDescription(): string
     {
